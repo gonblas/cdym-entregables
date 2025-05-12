2bd50a081b14d1d90fafd3f0a8c3570d05e83912
Ivan Polanis <polanisivan@gmail.com>
2025-05-12 12:01:05 -0300

    temporizador

--- tp2/TP2_Entregable_G9/TP2_Entregable_G9/mef.c
+++ tp2/TP2_Entregable_G9/TP2_Entregable_G9/mef.c
@@ -1,32 +1,32 @@
 /**
  * @file mef.c
- * @brief Implementación de una Máquina de Estados Finitos (MEF) para un juego de adivinanza de palabras.
- * 
- * Este archivo contiene la lógica de una MEF que permite al usuario adivinar palabras
- * seleccionadas aleatoriamente de un diccionario. El juego incluye estados para inicialización,
- * mostrar la palabra, esperar la entrada del usuario, manejar la victoria y manejar la derrota.
- * 
+ * @brief Implementación de una Máquina de Estados Finitos (MEF) para un juego
+ * de adivinanza de palabras.
+ *
+ * Este archivo contiene la lógica de una MEF que permite al usuario adivinar
+ * palabras seleccionadas aleatoriamente de un diccionario. El juego incluye
+ * estados para inicialización, mostrar la palabra, esperar la entrada del
+ * usuario, manejar la victoria y manejar la derrota.
+ *
  * @author Grupo 9
  * @date 2023
  */
 
+#include "keypad.h"
 #include "lcd.h"
 #include "lcd_out.h"
-#include "keypad.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-
-
 /**
  * @brief Maneja un error cometido por el usuario.
- * 
+ *
  * Incrementa el contador de errores, actualiza la pantalla y verifica si
  * el usuario ha alcanzado el límite de errores permitidos.
- * 
+ *
  * @param t Puntero a la variable de temporización.
  */
 void miss(uint8_t t);
@@ -40,14 +40,7 @@ void miss(uint8_t t);
 #define LAST_MESSAGE_TIME 5
 #define MAX_ERRORS 3
 
-typedef enum
-{
-  INIT,
-  SHOW_PASSWORD,
-  WAIT_INPUT,
-  VICTORY,
-  LOSE
-} state_t;
+typedef enum { INIT, SHOW_PASSWORD, WAIT_INPUT, VICTORY, LOSE } state_t;
 uint8_t *dictionary[] = {"Arbol", "Boton", "CDyMC", "ClavE", "Facil",
                          "Gafas", "Hojas", "LiBro", "Lanza", "Nieve",
                          "PeRro", "PecES", "PiAno", "PrYKe", "RUEDa",
@@ -61,26 +54,25 @@ uint8_t random_index, first;
 volatile uint8_t temporization_flag = 0;
 volatile uint8_t t = 0;
 
-void MEF_init()
-{
+void MEF_init() {
   state = INIT;
   srand(time(NULL));
   LCDclr();
 }
 
-void MEF_update()
-{
-  if (temporization_flag) return;
+void MEF_update() {
+  if (temporization_flag)
+    return;
+
+  temporization_flag = 0;
 
   KEYPAD_scan(&key);
 
-  switch (state)
-  {
+  switch (state) {
   case INIT:
     PRINT_word("Bienvenido", 0, 0);
     PRINT_word("Presione *", 0, 1);
-    if (key == SHOW_PASSWORD_KEY)
-    {
+    if (key == SHOW_PASSWORD_KEY) {
       state = SHOW_PASSWORD;
       strcpy(guess, "*****");
       errors = 0;
@@ -92,15 +84,13 @@ void MEF_update()
     }
     break;
   case SHOW_PASSWORD:
-    if (word == NULL)
-    {
+    if (word == NULL) {
       random_index = GET_RANDOM_INDEX(0, WORD_COUNT);
       word = dictionary[random_index];
       PRINT_word(word, 1, 0);
       t = 0;
     }
-    if (t == 0x02)
-    {
+    if (t == 0x02) {
       state = WAIT_INPUT;
       LCDclr();
       current_char = 0;
@@ -110,55 +100,44 @@ void MEF_update()
     }
     break;
   case WAIT_INPUT:
-    if (key == CHAR_END_KEY)
-    {
-      if (current_char == word[cur_char_index])
-      {
+    if (key == CHAR_END_KEY) {
+      if (current_char == word[cur_char_index]) {
         guess[cur_char_index] = current_char;
         current_char = 0;
         PRINT_guess(guess);
         cur_char_index++;
-        if (cur_char_index == 5)
-        {
+        if (cur_char_index == 5) {
           state = VICTORY;
           time_to_victory = t;
           t = 0;
         }
-      }
-      else
+      } else
         miss(t);
-    }
-    else if (key >= '0' && key <= '9')
-    {
+    } else if (key >= '0' && key <= '9') {
       if (UPDATE_CHAR(current_char, key) > 255)
         miss(t);
-      else
-      {
+      else {
         current_char = UPDATE_CHAR(current_char, key);
         PRINT_ascii_num(current_char);
       }
     }
     break;
   case VICTORY:
-    if (first == 1)
-    {
+    if (first == 1) {
       PRINT_victory(time_to_victory);
       first = 0;
     }
-    if (t == LAST_MESSAGE_TIME)
-    {
+    if (t == LAST_MESSAGE_TIME) {
       state = INIT;
       LCDclr();
     }
     break;
   case LOSE:
-    if (first == 1)
-    {
+    if (first == 1) {
       PRINT_lose();
       first = 0;
     }
-    if (t == LAST_MESSAGE_TIME)
-    {
+    if (t == LAST_MESSAGE_TIME) {
       state = INIT;
       LCDclr();
     }
@@ -166,12 +145,10 @@ void MEF_update()
   }
 }
 
-void miss(uint8_t t)
-{
+void miss(uint8_t t) {
   errors++;
   PRINT_error(errors);
-  if (errors == MAX_ERRORS)
-  {
+  if (errors == MAX_ERRORS) {
     state = LOSE;
     t = 0;
   }
--- tp2/TP2_Entregable_G9/TP2_Entregable_G9/timer.c
+++ tp2/TP2_Entregable_G9/TP2_Entregable_G9/timer.c
@@ -1,38 +1,39 @@
 /**
  * @file timer.c
- * @brief Implementación de un temporizador utilizando el Timer0 del microcontrolador AVR.
+ * @brief Implementación de un temporizador utilizando el Timer0 del
+ * microcontrolador AVR.
  *
- * Este archivo contiene las funciones necesarias para inicializar y manejar un temporizador
- * que genera interrupciones cada 10 ms. Además, se utiliza una variable de bandera para
- * realizar tareas temporizadas y un contador para medir intervalos de tiempo más largos.
+ * Este archivo contiene las funciones necesarias para inicializar y manejar un
+ * temporizador que genera interrupciones cada 10 ms. Además, se utiliza una
+ * variable de bandera para realizar tareas temporizadas y un contador para
+ * medir intervalos de tiempo más largos.
  *
  * @author Grupo 9
  * @date Fecha de entrega
  */
 
-
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include "mef.h"
 #include "timer.h"
+#include "mef.h"
+#include <avr/interrupt.h>
+#include <avr/io.h>
 
 uint8_t tick = 0;
 
 void timer_init() {
-  TCCR0A = 0x00; // Modo normal
+  TCCR0A = 0x00;                      // Modo normal
   TCCR0B = (1 << CS02) | (1 << CS00); // Prescaler = 1024
-  TCNT0 = 100; // Cuenta inicial para desbordar a 10ms
+  TCNT0 = 100;                        // Cuenta inicial para desbordar a 10ms
 
   TIMSK0 |= (1 << TOIE0); // Habilita interrupciones por overflow
-  sei(); // Habilita interrupciones globales
+  sei();                  // Habilita interrupciones globales
 }
 
 ISR(TIMER0_OVF_vect) {
-	TCNT0 = 100; // Reinicia el timer para 10 ms
-	tick++;
-	temporization_flag ^= 1;
-	if (tick == 100) { // Cada 100 ticks de 10 ms = 1 segundo
-		t++;
-		tick = 0;
-	}
+  TCNT0 = 100; // Reinicia el timer para 10 ms
+  tick++;
+  temporization_flag = 1;
+  if (tick == 100) { // Cada 100 ticks de 10 ms = 1 segundo
+    t++;
+    tick = 0;
+  }
 }
