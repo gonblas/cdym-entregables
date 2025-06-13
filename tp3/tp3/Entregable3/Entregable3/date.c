474aae2395d30321203d625c2d1a1d5697c8adc3
Ivan Polanis <polanisivan@gmail.com>
2025-06-13 14:28:40 -0300

    Update date formatting to include newline and increase buffer size

--- tp3/tp3/Entregable3/Entregable3/date.c
+++ tp3/tp3/Entregable3/Entregable3/date.c
@@ -164,8 +164,8 @@ uint8_t* format_time(date_t date) {
 }
 
 uint8_t* format_date(date_t date) {
-    static uint8_t formatted_date[20]; // "DD/MM/YY HH:MM:SS\0"
-    snprintf((char *)formatted_date, sizeof(formatted_date), "%02u/%02u/%02u %s",
+    static uint8_t formatted_date[22]; // "DD/MM/YY HH:MM:SS\0"
+    snprintf((char *)formatted_date, sizeof(formatted_date), "%02u/%02u/%02u %s\r\n",
              date.day, date.month, date.year, format_time(date));
     return formatted_date;
 }
