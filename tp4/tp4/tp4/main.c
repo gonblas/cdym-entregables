f0b6aa438cafc6ae84a80042d9608882db9962b8
Ivan Polanis <polanisivan@gmail.com>
2025-07-08 12:51:47 -0300

    Initialize Atmel Studio project files and add main application code

---
+++ tp4/tp4/tp4/main.c
@@ -0,0 +1,18 @@
+/*
+ * tp4.c
+ *
+ * Created: 7/8/2025 12:49:53 PM
+ * Author : Ivan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
